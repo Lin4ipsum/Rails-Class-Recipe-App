31a4d7b1c3cc326921c4f27980aef5b1a93ad652
Linda <lin.m.gonzalez@gmail.com>
2012-07-29 17:06:02 -0400

    Ingredient Names Should Be Normalized and Unique

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,16 @@
 class Ingredient < ActiveRecord::Base
   attr_accessible :name
+  
+  validates_uniqueness_of :name, :case_senstive => false
+  
+  before_save :normalize
+  
+  
+  private
+  
+  def normalize
+    self.name = name.mb_chars.downcase.strip.normalize
+  end
+  
+  
 end
