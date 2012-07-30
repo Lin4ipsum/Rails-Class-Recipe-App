d29e880e2c39d171a68301f08042129d5743d09f
Linda <lin.m.gonzalez@gmail.com>
2012-07-29 21:28:07 -0400

    7.5: added ability to edit ingredient and quantity from recipes/edit

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,10 +1,14 @@
 class Ingredient < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :recipe_ingredients_attributes
   
+  has_many :recipe_ingredients
+  has_many :recipes, :through => :recipe_ingredients
+ 
+  accepts_nested_attributes_for :recipe_ingredients
+ 
   validates_uniqueness_of :name, :case_senstive => false
   
   before_save :normalize
-  
   
   def normalize
     self.name = name.mb_chars.downcase.strip.normalize
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,9 +1,9 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :ingredients_attributes
   
   has_many :recipe_ingredients
   has_many :ingredients, :through => :recipe_ingredients
   
-  
+  accepts_nested_attributes_for :ingredients
   
 end
