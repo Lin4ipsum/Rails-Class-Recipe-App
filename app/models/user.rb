4d21b28d9f80f8666ca53228713d345394938d2c
Linda <lin.m.gonzalez@gmail.com>
2012-08-04 19:29:57 -0400

    Commit 12: User's have a Secured Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :first_name, :last_name
+  attr_accessible :email, :first_name, :last_name, :password, :password_confirmation
+
+  has_secure_password
+  validates_presence_of :password, :on => :create
+  
 end
